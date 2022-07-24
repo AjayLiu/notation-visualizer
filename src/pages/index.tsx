1500b599447d4ffd56892cb19103fb331adc26f3
Ajay Liu <ajayliu2928@gmail.com>
2022-07-24 00:33:04 -0700

    Prepare for publish

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,30 +8,33 @@ const Home: React.FC = () => {
             <GoogleAnalyticsHook />
             <div className="w-full bg-ajay text-white">
                 <Head>
-                    <title>Ajay Liu</title>
+                    <title>Notation Visualizer</title>
                     <link rel="icon" href="/favicon.ico" />
                     <meta
                         name="Description"
-                        content="My name is Ajay Liu and I love creating things!"
+                        content="An interactive website to visualize how Infix, Prefix (Polish), and Postfix (Reverse Polish) notation are converted and evaluated."
                     />
                     <meta
                         name="viewport"
                         content="width=device-width, initial-scale=1"
                     />
-                    <meta property="og:title" content="Ajay Liu" />
+                    <meta property="og:title" content="Notation Visualizer" />
                     <meta property="og:type" content="website" />
-                    <meta property="og:url" content="https://ajayliu.com/" />
+                    <meta
+                        property="og:url"
+                        content="https://notation-visualizer.ajayliu.com"
+                    />
                     <meta
                         property="og:image"
                         content="https://ajayliu.com/imgs/ajayliudark.png"
                     />
                     <meta
                         property="og:description"
-                        content="My name is Ajay Liu and I love creating things!"
+                        content="An interactive website to visualize how Infix, Prefix (Polish), and Postfix (Reverse Polish) notation are converted and evaluated."
                     />
                 </Head>
 
-                <main className="min-h-[40vh]"></main>
+                <main className="min-h-[60vh]"></main>
 
                 <Footer />
             </div>
