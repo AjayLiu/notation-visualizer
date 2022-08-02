1ab1391c4a3e400a5571d6a505931f69e3c75f48
Ajay Liu <ajayliu2928@gmail.com>
2022-08-02 14:56:46 -0700

    Tree should display the expression tree for the example in the textpanel

--- src/pages/tree.tsx
+++ src/pages/tree.tsx
@@ -4,7 +4,7 @@ import TreeVisualizer from "@components/TreeVisualizer";
 const TreePage: React.FC = () => {
     return (
         <Layout title="Tree - Notation Visualizer">
-            <TreeVisualizer initialExpression="43 7 123 * + 6 9 - /" />
+            <TreeVisualizer initialExpression="8 2 3 * - 7 +" />
         </Layout>
     );
 };
